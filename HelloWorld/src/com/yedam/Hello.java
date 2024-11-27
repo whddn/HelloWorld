b5865c2701233563a9a2e3e6314d0047d453e23b
whddn <ghjkl133120@naver.com>
2024-11-27 17:48:59 +0900

    자바 시작

---
+++ HelloWorld/src/com/yedam/Hello.java
@@ -0,0 +1,15 @@
+package com.yedam;
+
+// 대소문자 구분 (A , a 서로 다
+public class Hello {
+	// main 메소드.
+	public static void main(String[] args) {
+		// 콘솔에 Hello, World 출력
+		System.out.println("Hello, World");
+		
+		// Hello.java -> Hello.class(실행파일)
+		// 컴파일(저장하면 eclipse 컴파일)
+		// HelloWorld 프로젵그 - HelloWorld repository
+		
+	}
+}
