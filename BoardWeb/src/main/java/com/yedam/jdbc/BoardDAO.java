4924d957137102438f1183a04536f9c4c6539c01
whddn <ghjkl133120@naver.com>
2024-12-09 17:47:57 +0900

    Servlet 첫 시작

---
+++ BoardWeb/src/main/java/com/yedam/jdbc/BoardDAO.java
@@ -0,0 +1,97 @@
+package com.yedam.jdbc;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.yedam.common.DAO;
+import com.yedam.vo.BoardVO;
+
+public class BoardDAO extends DAO {
+
+	// 상세조회
+	// 파라미터(int boardNo) selectBoard 반환값: BoardVO.
+	public BoardVO selectBoard(int boardNO) {
+		getConn();
+		String sql = "select * from tbl_board where board_no = ?";
+		
+		try {
+			psmt = conn.prepareStatement(sql);
+			psmt.setInt(1, boardNO);
+			rs = psmt.executeQuery();
+
+			while (rs.next()) {
+				BoardVO brd = new BoardVO();
+				brd.setBoardNo(rs.getInt("board_no"));
+				brd.setTitle(rs.getString("title"));
+				brd.setContent(rs.getString("content"));
+				brd.setWriter(rs.getString("writer"));
+				brd.setViewCnt(rs.getInt("view_cnt"));
+				brd.setCreationDate(rs.getDate("creation_date"));
+				brd.setUpdateDate(rs.getDate("update_date"));
+				
+				return brd;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			disConnect();
+		}
+		return null;
+	}
+
+	// BoardVO 파라미터 => 등록
+	public boolean insertBoard(BoardVO board) {
+		getConn();
+		String sql = "insert into tbl_board " + "(board_no, title, content, writer) "
+				+ "values(board_seq.nextval, ?, ?, ?) ";
+
+		try {
+			psmt = conn.prepareStatement(sql);
+
+			psmt.setString(1, board.getTitle());
+			psmt.setString(2, board.getContent());
+			psmt.setString(3, board.getWriter());
+			int r = psmt.executeUpdate(); // 쿼리실행
+			if (r > 0) {
+				return true;
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			disConnect();
+		}
+		return false;
+	}
+
+	// 목록
+	public List<BoardVO> boardList() {
+		getConn();
+		String sql = "select * from tbl_board order by board_no";
+		List<BoardVO> result = new ArrayList<>();
+
+		try {
+			psmt = conn.prepareStatement(sql);
+			rs = psmt.executeQuery();
+
+			while (rs.next()) {
+				BoardVO brd = new BoardVO();
+				brd.setBoardNo(rs.getInt("board_no"));
+				brd.setTitle(rs.getString("title"));
+				brd.setContent(rs.getString("content"));
+				brd.setWriter(rs.getString("writer"));
+				brd.setViewCnt(rs.getInt("view_cnt"));
+				brd.setCreationDate(rs.getDate("creation_date"));
+				brd.setUpdateDate(rs.getDate("update_date"));
+
+				result.add(brd);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			disConnect();
+		}
+		return result;
+	}
+}
---
+++ BoardWeb/src/main/java/com/yedam/vo/BoardVO.java
@@ -0,0 +1,27 @@
+package com.yedam.vo;
+
+import java.util.Date;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/*
+ * VO: Value Object
+ * Lombok 사용: 1. Lombok 다운로드 설치
+ * 			   2. Lombok 라이브러리 다운로드
+ * 			   3. eclipse 재실행
+ * 			   4. 어노테이션
+ */
+@Data
+
+public class BoardVO {
+	private int boardNo; //board_no => boardNo 형식
+	private String title;
+	private String content;
+	private String writer;
+	private int viewCnt;
+	private Date creationDate;
+	private Date updateDate;
+}
